4d60538a953d342e958929222cd597cc2af0f7d8
AlexanderDLe <alexanderledev@gmail.com>
2020-01-03 22:14:18 -0800

    Convert to vanilla JS

---
+++ server/client/src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
+
+class App extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Header />
+                    <div className="container">
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route
+                            exact
+                            path="/surveys/new"
+                            component={SurveyNew}
+                        />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, { fetchUser })(App);
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+const bodyParser = require('body-parser');
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
+///
+mongoose
+    .connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB running...'))
+    .catch(err => console.error(err));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Ensure express serves production assets
+    // like main.js file, or main.css file
+    const path = require('path');
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    // Previously: app.use(express.static('/client/build'));
+
+    // Ensure express serves index.html file
+    // if it doesn't recognize the route
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
